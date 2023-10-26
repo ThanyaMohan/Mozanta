0ce8514116dd1802cddb0cfa7163a997315734bf
ThanyaMohan <148606626+ThanyaMohan@users.noreply.github.com>
2023-10-26 14:37:22 +0530

    Create student1.java

---
+++ student1.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/demo/model/Student.java
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private String dateOfBirth;
+    private String studentClass;
+    private String division;
+    private String gender;
+}
