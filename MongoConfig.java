b77e491eceabdf87cc231f808f89e812a108c03e
ThanyaMohan <148606626+ThanyaMohan@users.noreply.github.com>
2023-10-26 14:38:50 +0530

    Create MongoConfig.java

---
+++ MongoConfig.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "your_database_name";
+    }
+}
